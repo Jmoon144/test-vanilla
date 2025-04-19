a4e6b15f0c6352f135f3e3a51c3ff4a43fe5f88c
Jmoon144 <sjwm98@gmail.com>
2025-04-19 10:56:01 +0900

    feat: 초기 index.ts 진입점 구성

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const root = document.getElementById("app");
+if (root) {
+  const title = document.createElement("h1");
+  title.textContent = "Vanilla JS + Webpack 환경 완성!";
+  root.appendChild(title);
+}
